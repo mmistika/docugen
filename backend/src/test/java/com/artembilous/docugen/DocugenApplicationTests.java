b64b7f16d2a0a3fc70b34a37349a4323e5bb028e
Artem Bilous <mistikasoft@gmail.com>
2026-03-15 11:03:52 +0200

    initial backend project

---
+++ backend/src/test/java/com/artembilous/docugen/DocugenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artembilous.docugen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocugenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
