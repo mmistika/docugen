eb2651c1616536497a80ed84cd92ed82df14ac6c
Artem Bilous <mistikasoft@gmail.com>
2026-04-24 23:16:11 +0200

    feat(frontend): implement RoleModal comp

---
+++ frontend/src/types/rbac.ts
@@ -0,0 +1,14 @@
+export interface PermissionDTO {
+    id: number
+    name: string
+}
+
+export interface RoleDTO {
+    name: string
+    permissions: string[]
+}
+
+export interface RoleUpdateRequest {
+    name: string
+    permissions: string[]
+}
