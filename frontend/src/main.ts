1757911b0715c61851810a1750d0edbc0bc5e07a
Artem Bilous <mistikasoft@gmail.com>
2026-05-11 11:32:46 +0200

    style(frontend): code formatting

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,11 @@
-import "./assets/main.css";
+import './assets/main.css';
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
-import { auth0 } from "@/auth";
+import App from './App.vue';
+import router from './router';
+import { auth0 } from '@/auth';
 
 const app = createApp(App);
 
