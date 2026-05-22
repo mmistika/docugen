b4a94a577a9836a99b836a734ed18f3e4a8e17f5
Artem Bilous <mistikasoft@gmail.com>
2026-05-22 20:19:31 +0200

    chore(frontend): install and configure vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
@@ -12,5 +12,8 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    test: {
+        environment: 'jsdom'
     }
 });
